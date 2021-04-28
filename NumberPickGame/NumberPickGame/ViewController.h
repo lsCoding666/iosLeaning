139304179e963827693c38b4233f1197b8407309
reburnliang <reburnliang@tencent.com>
2021-04-28 20:42:46 +0800

    feat(NumberPickGame):完成游戏的设计

--- NumberPickGame/NumberPickGame/ViewController.h
+++ NumberPickGame/NumberPickGame/ViewController.h
@@ -12,5 +12,10 @@
 @property (strong, nonatomic) UITextField *minTextField;
 @property (strong, nonatomic) UITextField *maxTextField;
 @property (strong, nonatomic) UITextField *targetValueTextField;
+@property (strong, nonatomic) UITextField *scoreTextField;
+//回合数
+@property (strong, nonatomic) UITextField *numOfRoundsTextField;
+//本轮回合的尝试次数
+@property (strong, nonatomic) UITextField *numOfTryTextField;
 @end
 
--- NumberPickGame/NumberPickGame/ViewController.m
+++ NumberPickGame/NumberPickGame/ViewController.m
@@ -12,6 +12,9 @@
     int screenWidth;
     int screenHeight;
     int targetValue;
+    int countOfTry;
+    int score;
+    int rounds;
 }
 -(IBAction)showAlert:(UISlider*)sender;
 - (IBAction)sliderMoved:(id)sender;
@@ -21,6 +24,9 @@
 @synthesize slider = _slider;
 @synthesize minTextField = _minTextField;
 @synthesize targetValueTextField = _targetValueTextField;
+@synthesize scoreTextField =_scoreTextField;
+@synthesize numOfRoundsTextField = _numOfRoundsTextField;
+@synthesize numOfTryTextField = _numOfTryTextField;
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
@@ -39,11 +45,30 @@
  下一轮游戏
  */
 -(void)startNewRound{
+    //本轮尝试次数置0
+    countOfTry = 0;
     [self getNewTargetNum];
     currentValue = 50;
     _slider.value = currentValue;
     [self textAlignCenterhorizontal:[NSString stringWithFormat:@"%d", targetValue] :_targetValueTextField];
+    [self showCountOfTry];
     //[_targetValueTextField setText:[NSString stringWithFormat:@"%d", targetValue]];
+}
+
+/**
+ 计算得分
+ */
+-(void)countScore{
+    if (countOfTry==1) {
+        score += 10;
+    }else if (countOfTry < 3){
+        score += 5;
+    }else if (countOfTry < 5){
+        score += 3;
+    }else{
+        score += 1;
+    }
+    [_scoreTextField setText:[NSString stringWithFormat:@"%d", score]];
 }
 
 
@@ -105,16 +130,16 @@
     
     //按钮 显示slider的当前值
     UIButton *showSliderValueButton = [[UIButton alloc]initWithFrame:CGRectMake(screenWidth/6*5, screenHeight/6*5, 100, 30)];
-    [showSliderValueButton setTitle:@"显示结果" forState:UIControlStateNormal];
+    [showSliderValueButton setTitle:@"提交" forState:UIControlStateNormal];
     //设置点击事件
     [showSliderValueButton addTarget:self action:@selector(showAlert:) forControlEvents:UIControlEventTouchDown];
     
     
     //再来一次按钮
-    UIButton *tryAgainBtn = [[UIButton alloc]initWithFrame:CGRectMake(screenWidth/15, screenHeight/6*5, 100, 30)];
-    [tryAgainBtn setTitle:@"再来一次" forState:UIControlStateNormal];
+    UIButton *tryAgainBtn = [[UIButton alloc]initWithFrame:CGRectMake(screenWidth/18, screenHeight/6*5, 150, 30)];
+    [tryAgainBtn setTitle:@"重置分数和回合数" forState:UIControlStateNormal];
     //设置点击事件
-    //[tryAgainBtn addTarget:self action:@selector(showAlert:) forControlEvents:UIControlEventTouchDown];
+    [tryAgainBtn addTarget:self action:@selector(resetRounds:) forControlEvents:UIControlEventTouchDown];
     
     
     //分数 提示“分数：”
@@ -124,7 +149,8 @@
     
     //分数 真正的得分
     UITextField *scoreTextField = [[UITextField alloc]initWithFrame:CGRectMake(screenWidth/15*5, screenHeight/6*5, 50, 30)];
-    [scoreTextField setText:@"80分"];
+    [scoreTextField setText:@"0"];
+    self.scoreTextField = scoreTextField;
     
     
     //回合数提示
@@ -134,8 +160,14 @@
     
     //回合数
     UITextField *countOfRoundsTextField = [[UITextField alloc]initWithFrame:CGRectMake(countOfRoundsHintTextField.frame.origin.x+countOfRoundsHintTextField.frame.size.width+5, screenHeight/6*5, 100, 30)];
-    [countOfRoundsTextField setText:@"10"];
+    [countOfRoundsTextField setText:@"0"];
+    self.numOfRoundsTextField = countOfRoundsTextField;
     
+    
+    //尝试次数展示
+    UITextField *countOfTryTextField = [[UITextField alloc]initWithFrame:CGRectMake(screenWidth/15*10, screenHeight/6*5, 170, 30)];
+    [countOfTryTextField setText:@"尝试：0次"];
+    self.numOfTryTextField = countOfTryTextField;
     
     //目标值
     UITextField *targetValueTextField = [[UITextField alloc]initWithFrame:CGRectMake(0, goalText.frame.origin.y+35, screenWidth, 30)];
@@ -155,34 +187,61 @@
     [self.view addSubview:countOfRoundsHintTextField];
     [self.view addSubview:countOfRoundsTextField];
     [self.view addSubview:targetValueTextField];
+    [self.view addSubview:countOfTryTextField];
 }
 
 /**
- slider通知
+ 计算回合数量
  */
-- (IBAction)sliderMoved:(UISlider*)sender {
-    NSLog(@"滑动条当前的数值是:%f",sender.value);
-    currentValue =(int) lroundf(sender.value);
+-(void)countRounds{
+    [_numOfRoundsTextField setText:[NSString stringWithFormat:@"%d",rounds]];
 }
+
+/**
+ 重制回合数和尝试次数
+ */
+-(IBAction)resetRounds:(id)sender{
+    rounds = 0;
+    score = 0;
+    [_scoreTextField setText:@"0"];
+    [self countRounds];
+    [self startNewRound];
+}
+
+-(void)showCountOfTry{
+    [_numOfTryTextField setText:[NSString stringWithFormat:@"尝试：%d次",countOfTry]];
+}
+
 
 /**
  点击显示当前值按钮弹出对话框提示当前值
  */
 -(IBAction)showAlert:(id)sender{
     currentValue = _slider.value;
+    countOfTry += 1;
+    rounds += 1;
+    [self showCountOfTry];
+    [self countRounds];
     NSString *message = [NSString stringWithFormat:@"滑动条的当前数值是: %d,我们的目标数值是：%d",currentValue,targetValue];
     // 1.创建UIAlertController
     UIAlertController *alertController = [UIAlertController alertControllerWithTitle:@"Alert Title"
                                                             message:message
                                                             preferredStyle:UIAlertControllerStyleAlert];
     // 2.创建并添加按钮
-        UIAlertAction *okAction = [UIAlertAction actionWithTitle:@"下一轮" style:UIAlertActionStyleDefault handler:^(UIAlertAction * _Nonnull action) {
+        UIAlertAction *okAction = [UIAlertAction actionWithTitle:@"直接下一轮" style:UIAlertActionStyleDefault handler:^(UIAlertAction * _Nonnull action) {
             NSLog(@"OK Action");
-            [self startNewRound];
-            
+            if (targetValue==currentValue) {
+                [self countScore];
+            }
+                [self startNewRound];
+
         }];
         UIAlertAction *cancelAction = [UIAlertAction actionWithTitle:@"继续" style:UIAlertActionStyleCancel handler:^(UIAlertAction * _Nonnull action) {
             NSLog(@"Cancel Action");
+            if (targetValue==currentValue) {
+                [self countScore];
+                [self startNewRound];
+            }
         }];
 
         [alertController addAction:okAction];           // A
